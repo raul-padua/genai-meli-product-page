053bea4ec3e5d4cb631c06c508d4ee512ebbf8c6
Raul Salles de Padua <raulsallesdepadua@Rauls-MacBook-Pro.local>
2025-10-28 19:22:21 -0400

    Switch to Next.js API proxy approach - frontend proxies to separate backend

--- api/simple.py
+++
@@ -1,7 +0,0 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": '{"message": "Hello from Vercel!"}'
-    }
-
