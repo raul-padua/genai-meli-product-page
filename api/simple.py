d0823dad4156827385b31ccfc2269515971f3168
Raul Salles de Padua <raulsallesdepadua@Rauls-MacBook-Pro.local>
2025-10-28 19:20:50 -0400

    Test simpler handler

---
+++ api/simple.py
@@ -0,0 +1,7 @@
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": '{"message": "Hello from Vercel!"}'
+    }
+
